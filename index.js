52fa001506204818ec750d11d3b68ede3568af37
Adam Dvořák <adam.dvorak@gmail.com>
2015-02-25 21:14:11 +0100

    changed uri, definitive one

--- index.js
+++ index.js
@@ -24,16 +24,19 @@ temporal.loop(60000*5, function() {
   console.log("[+] repeating 5 min");
 
   request({
-     uri: "http://www.cic.gc.ca/english/work/iec/index.asp",
+     //uri: "http://www.cic.gc.ca/english/work/iec/index.asp",
+     uri: "http://kompass-2015-iec-eic.international.gc.ca/IECRegistrationClosed.aspx?regionCode=cz",
      }, function(error, response, body) {
      var $ = cheerio.load(body);
 
-     toFind = $("div.alert.alert-info >p").html();
+     //toFind = $("div.alert.alert-info >p").html();
      //toFind = $("div.alert.alert-info >pp").html(); // should be null
+     toFind = $("span#lblRegClosedMain").html(); //changing uri, this is cardinal one
 
-     console.log("[p tag value]: " + toFind )
+     console.log("[searched value]: " + toFind )
      // on the end is _space_ 
-     closedText = "The International Experience Canada (IEC) 2015 season will not open before mid-February 2015. ";
+     //closedText = "The International Experience Canada (IEC) 2015 season will not open before mid-February 2015. ";
+     closedText = 'The 2015 International Experience Canada (IEC) initiative with Czech Republic is currently closed, and will re-open shortly.  We encourage you to check the updates on our website regularly.';
 
     // it's enough to check this - we don't need: if (null == toFind) { console.log("je null") }
     if (toFind != closedText) {
@@ -44,7 +47,7 @@ temporal.loop(60000*5, function() {
       console.log("IEC still CLOSED..");
     }
   })
- 
+  // this will not work on Heroku
   if (opened === 5) { // pls no more then 5 notifications
     console.log("STOPPING!");
     this.stop();
